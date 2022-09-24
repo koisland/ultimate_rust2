9f7c2f7c1cc595ad9391c0913fc2e36203804a64
Keith <koshima789@gmail.com>
2022-09-24 00:11:01 -0700

    Finished closures_iterators, docs, and idiomatic rust.

--- exercise/closures_iterators/src/main.rs
+++ exercise/closures_iterators/src/main.rs
@@ -6,31 +6,45 @@ fn main() {
     // number multiplied by itself), and assign the closure to the "square" variable. Then run the
     // code and make sure it works.
 
-    // let square = ...
-    // println!("5 squared is {}", square(5));
+    let square = |x| x * x; 
+    println!("1. 5 squared is {}.\n", square(5));
 
     // 2. Uncomment the code below.  Finish the .map() iterator adaptor call by passing it a closure
     // which takes a tuple of two integers as a parameter, and returns a tuple with the first
     // integer incremented by 1, and the second integer left alone.  For example, if given the input
     // (0, 1), it should return (1, 1). Run the code and make sure it works.
 
-    // let pairs = vec![(0, 1), (2, 3), (4, 5)];
-    // pairs
-    //     .into_iter()
-    //     .map( ... )
-    //     .for_each(|t| println!("{:?}", t));
+    let pairs = vec![(0, 1), (2, 3), (4, 5)];
+    let pairs_copy = pairs.clone();
+    println!("2. Functional version:");
+    pairs
+        .into_iter()
+        .map(|(x, y)| (x + 1, y))
+        .for_each(|t| println!("{:?}", t));
+
+    println!("\nChallenge. For loop version:");
+    for (pair_1, pair_2) in pairs_copy.iter() {
+        println!("({}, {})", pair_1 + 1, pair_2)
+    }
 
     // 3. Uncomment the code below. There is a mutable vector named `numbers`. Use an iterator over
     // mutable references to multiply each of the values in `numbers` by 3.
     // Hint 1: You'll need .iter_mut() -- bonus points if you use the shorter, syntactic sugar form!
     // Hint 2: `x` will be a mutable reference, so remember to dereference it to use it
 
-    // let mut numbers = vec![1, 2, 3, 4];
-    // for x in ... {
-    //     ... // multiply the value by 3 via the mutable reference x
-    // }
-    // println!("{:?}", numbers); // should print [3, 6, 9, 12]
+    let mut numbers = vec![1, 2, 3, 4];
+    let mut numbers_copy = numbers.clone();
+    for x in &mut numbers {
+        *x = &*x * 3 // multiply the value by 3 via the mutable reference x
+    }
+    println!("\n3. {:?}\n", numbers); // should print [3, 6, 9, 12]
 
+    println!("Challenge. Functional version.");
+    let numbers_modified: Vec<i32> = numbers_copy
+        .iter_mut()
+        .map(|num| *num * 3)
+        .collect();
+    println!("{:?}\n", numbers_modified);
     // 4. Uncomment the code below.  Take the vector of words and
     // - Convert the vector into an iterator with .into_iter()
     // - Use .filter() to remove any word that contains the letter "h" -- use .contains()
@@ -39,9 +53,13 @@ fn main() {
     //
     // Hint: .to_uppercase() is a method on `str` which returns a String
 
-    // let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
-    // let transformed...  // do the stuff here
-    // println!("Transformed: {:?}", transformed);
+    let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
+    let transformed: Vec<String> = words
+        .into_iter()
+        .filter(|word| !word.contains("h"))
+        .map(|word| word.to_uppercase())
+        .collect();  // do the stuff here
+    println!("4. Transformed: {:?}", transformed);
 
     // Challenge:
     //
--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -4,6 +4,9 @@
 //
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
+//! A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep yellow to orange in coloration. The thick shell contains the seeds and pulp. The name is most commonly used for cultivars of Cucurbita pepo, but some cultivars of Cucurbita maxima, C. argyrosperma, and C. moschata with similar appearance are also sometimes called "pumpkins"
+//! ![](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
@@ -15,8 +18,14 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+/// A big orange thing.
+/// 
+/// # Recipes
+/// *Coming soon!*
 pub struct Pumpkin {
+    /// The roundess describes the shape (percentage) of a pumpkin.
     pub roundness: f32,
+    /// The orangeness describes the brightness (number from 8 - 27) of the pumpkin.
     pub orangeness: i32,
 }
 
@@ -24,12 +33,15 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// This method destroys the pumpkin and prevents its use for pie.
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+
+/// The color BURNT_ORANGE maps to the organgess field of the [Pumpkin struct](Pumpkin)
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
